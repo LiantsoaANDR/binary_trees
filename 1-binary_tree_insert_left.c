69b5d3af3c5773b723e8b2e46596479f5b37fb99
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-01-29 11:29:14 -0800

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,19 +13,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	child = malloc(sizeof(binary_tree_t));
+	child = binary_tree_node(parent, value);
 	if (!child)
 		return (NULL);
 
-	child = binary_tree_node(parent, value);
-
 	if (parent->left)
+	{
 		old = parent->left;
+		old->parent = child;
+	}
 
 	parent->left = child;
-
-	if (old)
-		child->left = old;
+	child->left = old;
 
 	return (child);
 }
