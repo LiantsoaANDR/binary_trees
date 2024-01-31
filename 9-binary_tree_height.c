c3dc5d1e96f4411dab10889a4553cf038fb42936
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-01-31 01:01:28 -0800

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_h = 0, right_h = 0;
 
-	if (!tree)
+	if (!tree || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	left_h = binary_tree_height(tree->left);
