bac43de6e0564781ca648095369907dab4f5d8ba
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-01-31 09:46:43 -0800

    Task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,16 +2,23 @@
 /**
  * binary_tree_is_full - Checks if a binary tree is full
  * @tree: a pointer to the root node of the tree to check
- * Return: 1 if a branch has 2 children (left and right)
- * Otherwise 0
+ * Return: 1 if the tree is full, otherwise 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int is_full = 1;
+
 	if (!tree)
 		return (0);
 
-	if (tree->left && tree->right)
+	if (!(tree->left) && !(tree->right))
 		return (1);
 
-	return (0);
+	if (tree->left && tree->right)
+	{
+		is_full *= binary_tree_is_full(tree->left);
+		is_full *= binary_tree_is_full(tree->right);
+	}
+
+	return (is_full);
 }
